3f933af4f750a2eaefddf6971447bb8d91f8b81c
BinbinTang <tangbbnus@gmail.com>
2014-10-04 15:45:56 +0800

    Create FAT32ImageUnpacker class

--- EagleEye/src/eagleeye/datacarving/unpack/FAT32ImageUnpacker.java
+++ EagleEye/src/eagleeye/datacarving/unpack/FAT32ImageUnpacker.java
@@ -1,13 +1,16 @@
 package eagleeye.datacarving.unpack;
 
+import java.util.ArrayList;
+
+import eagleeye.entities.File;
 import eagleeye.filesystem.format.FormatDescription;
 
 public class FAT32ImageUnpacker implements IDiskImageUnpacker{
 
 	@Override
-	public boolean unpack(FormatDescription format) throws Exception {
+	public ArrayList<File> unpack(FormatDescription format) throws Exception {
 		// TODO Auto-generated method stub
-		return false;
+		return null;
 	}
 
 }
