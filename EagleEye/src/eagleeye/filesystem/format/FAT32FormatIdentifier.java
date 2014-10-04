42a6aee4dd48eb82231977f8d72c1ace12a177c7
BinbinTang <tangbbnus@gmail.com>
2014-10-04 20:24:50 +0800

    Create Class for FAT32FormatIdentifier

---
+++ EagleEye/src/eagleeye/filesystem/format/FAT32FormatIdentifier.java
@@ -0,0 +1,13 @@
+package eagleeye.filesystem.format;
+
+import java.io.File;
+
+public class FAT32FormatIdentifier implements IFormatIdentifier {
+
+	@Override
+	public FormatDescription identify(File file) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
