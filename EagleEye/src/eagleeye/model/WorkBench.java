4e1983bd207626624e9c5a23b0b4c3404d07335c
Caster123 <magic.ken23@gmail.com>
2014-10-09 14:28:11 +0800

    Rearrange Search and time, and some other minor modifications

---
+++ EagleEye/src/eagleeye/model/WorkBench.java
@@ -0,0 +1,45 @@
+package eagleeye.model;
+
+import java.util.Calendar;
+
+public class WorkBench {
+	
+	private Calendar startDate, endDate;
+	
+	/**
+	 * default constructor
+	 */
+	
+	public WorkBench(){
+		
+	}
+	
+	/**
+	 * UI internal functions
+	 */
+	
+	//Date picker methods
+	
+	public String pickStartDate(){
+		String resultDate = "";
+		
+		return resultDate;
+	}
+	
+	public String pickEndDate(){
+		String resultDate = "";
+		
+		return resultDate;
+		
+	}
+	
+	/**
+	 * UI binding functions
+	 */
+	
+	
+	/**
+	 * Data Retrieving functions 
+	 */
+	
+}
