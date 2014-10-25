381de9c06ab547538fbefd8c410a8b57b9c1b11f
Caster <Caster@192.168.1.21>
2014-10-25 17:10:54 +0800

    GUI modified

--- EagleEye/src/eagleeye/controller/MainApp.java
+++ EagleEye/src/eagleeye/controller/MainApp.java
@@ -36,8 +36,10 @@ public class MainApp extends Application {
             Scene scene = new Scene(rootLayout, 50 * rem, 40 * rem);
             //Scene scene = new Scene(rootLayout);
 
-            primaryStage.setMinHeight(600);
-            primaryStage.setMinWidth(800);
+            primaryStage.setMinHeight(400);
+            primaryStage.setMinWidth(820);
+            primaryStage.setHeight(600);
+            primaryStage.setWidth(820);
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (IOException e) {
@@ -66,7 +68,8 @@ public class MainApp extends Application {
             FXMLLoader loader = new FXMLLoader(MainApp.class.getResource("../view/WorkBench.fxml"));
             AnchorPane overviewPage = (AnchorPane) loader.load();
             rootLayout.setCenter(overviewPage);
-            
+            overviewPage.setMinHeight(400);
+            overviewPage.setMinWidth(800);
             WorkBenchController controller = loader.getController();
             controller.setMainApp(this);
 
