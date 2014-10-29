75f0aa6e5754db53e2cf5283b8116f3b4e097536
AvanCaiJun <avanchua@live.com>
2014-10-29 14:50:41 +0800

    Filters Done: Category, Keyword, dates, time of the day, ismodified or is recovered
    New db model also up

--- EagleEye/src/eagleeye/dbcontroller/DBQueryController.java
+++ EagleEye/src/eagleeye/dbcontroller/DBQueryController.java
@@ -1,6 +1,7 @@
 package eagleeye.dbcontroller;
 
 import eagleeye.entities.*;
+
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -164,7 +165,61 @@ public class DBQueryController {
 		
 	}
 	
-	//Helper method
+	public ArrayList<FileEntity> getFilteredFiles(Filter filter) {
+		
+		boolean isKeywordPresent = isKeywordPresent(filter.getKeyword());
+		boolean isCategoryPresent = isCategoryPresent(filter.getCategoryName());
+		ArrayList<FileEntity> listOfFiles = new ArrayList<FileEntity>();
+		
+		Connection conn = DBConnection.dbConnector();
+		
+		try {
+			
+			PreparedStatement stmt = conn.prepareStatement(queryMaker.getFilteredFiles(isKeywordPresent, isCategoryPresent));
+			stmt = setFieldsForFilter(stmt,isKeywordPresent,isCategoryPresent,filter);
+			
+			ResultSet rs = stmt.executeQuery();
+			
+			while(rs.next()){
+				
+				FileEntity f = new FileEntity();
+			
+				f.modifyFileName(rs.getString("FileName"));
+				f.modifyFileID(rs.getInt("FileID"));
+				f.modifyDirectoryID(rs.getInt("DirectoryID"));
+				f.modifyDirectoryName(rs.getString("DirectoryName"));
+				f.modifyFileExt(rs.getString("FileExt"));
+				f.modifyFileExtID(rs.getInt("FileExtID"));
+				f.modifyDateCreated(rs.getString("DateCreated"));
+				f.modifyDateAccessed(rs.getString("DateAccessed"));
+				f.modifyDateModified(rs.getString("DateModified"));
+				f.modifyIsModified(rs.getBoolean("IsModified"));
+				f.modifyModifiedExt(rs.getString("ModifiedExt"));
+				f.modifyIsRecovered(rs.getBoolean("IsRecovered"));
+				f.modifyDateDeleted(rs.getString("DateDeleted"));
+				f.modifyFilePath(rs.getString("FilePath"));
+				f.modifyCategory(rs.getString("ExtTypeName"));
+								
+				listOfFiles.add(f);
+			}
+			
+			conn.close();
+		} catch (Exception e) {
+			
+			System.out.println("Filter Query Failed");
+			System.out.println(e.getMessage());
+			try {
+				conn.close();
+			} catch (Exception e2) {
+				
+			}
+		}
+		
+		return listOfFiles;
+		
+	}
+	
+	//HELPER METHODS SECTION
 	private ArrayList<Directory> organizeFilesAndDirectory (ArrayList<Directory> listOfDirectory, ArrayList<FileEntity> listOfFiles){
 		
 		for(FileEntity f : listOfFiles) {
@@ -183,6 +238,81 @@ public class DBQueryController {
 		}
 		
 		return listOfDirectory;
+	}
+	
+	private PreparedStatement setFieldsForFilter(PreparedStatement stmt,boolean isKeywordPresent,boolean isCategoryPresent, Filter filter) throws SQLException {
+		
+		if(isKeywordPresent && isCategoryPresent) {
+			
+			stmt.setInt(1,deviceID);
+			stmt.setString(2, filter.getKeyword());
+			stmt.setInt(3, filter.getCategoryID());
+			stmt.setString(4, filter.getStartDateAsString());
+			stmt.setString(5, filter.getEndDateAsString());
+			stmt.setString(6, filter.getStartTime());
+			stmt.setString(7, filter.getEndTime());
+			stmt.setInt(8, filter.getIsModified());
+			stmt.setInt(9, filter.getIsRecovered());
+						
+		} else {
+			
+			if(isKeywordPresent) {
+				
+				stmt.setInt(1,deviceID);
+				stmt.setString(2, filter.getKeyword());
+				stmt.setString(3, filter.getStartDateAsString());
+				stmt.setString(4, filter.getEndDateAsString());
+				stmt.setString(5, filter.getStartTime());
+				stmt.setString(6, filter.getEndTime());
+				stmt.setInt(7, filter.getIsModified());
+				stmt.setInt(8, filter.getIsRecovered());
+			
+			} else if (isCategoryPresent) {
+				
+				stmt.setInt(1,deviceID);
+				stmt.setInt(2, filter.getCategoryID());
+				stmt.setString(3, filter.getStartDateAsString());
+				stmt.setString(4, filter.getEndDateAsString());
+				stmt.setString(5, filter.getStartTime());
+				stmt.setString(6, filter.getEndTime());
+				stmt.setInt(7, filter.getIsModified());
+				stmt.setInt(8, filter.getIsRecovered());
+			
+			} else {
+				
+				stmt.setInt(1,deviceID);
+				stmt.setString(2, filter.getStartDateAsString());
+				stmt.setString(3, filter.getEndDateAsString());
+				stmt.setString(4, filter.getStartTime());
+				stmt.setString(5, filter.getEndTime());
+				stmt.setInt(6, filter.getIsModified());
+				stmt.setInt(7, filter.getIsRecovered());
+			}
+			
+		}
+		
+		return stmt;
+		
+	}
+	
+	private boolean isKeywordPresent(String keyword) {
+		
+		String emptyString = "";
+		
+		if (keyword.equals(emptyString))
+			return false;
+		else
+			return true;
+	}
+	
+	private boolean isCategoryPresent(String categoryName) {
+		
+		String emptyString = "";
+		
+		if (categoryName.equals(emptyString))
+			return false;
+		else
+			return true;
 		
 	}
 }
