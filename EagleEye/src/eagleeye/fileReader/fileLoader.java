10b035735b9eff84e6bf40928560625c6ea62fd0
Caster123 <magic.ken23@gmail.com>
2014-11-17 13:59:28 +0800

    Viewer update

--- EagleEye/src/eagleeye/fileReader/fileLoader.java
+++ EagleEye/src/eagleeye/fileReader/fileLoader.java
@@ -2,14 +2,18 @@ package eagleeye.fileReader;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
@@ -21,36 +25,26 @@ public class fileLoader
 {
 	private CustomPanel paintingPanel;
 
-	
-	
+
+
 	public String path=null;
 	public String content=null;
-	
+
 	static int ListOfFormatSize=3;
 	private static LinkedList<String> listOfFormat=new LinkedList();
 
 	public static  boolean isPhoto=false;
 	public static  boolean isText=false;
-	/*   private ActionListener timerAction = new ActionListener()
-    {
-        public void actionPerformed(ActionEvent ae)
-        {
-            x++;
-            y++;
-            paintingPanel.setPosition(x, y);
-        }
-    };*/
 
-	 //return true if format is txt or any of the three photo formats
 	public static boolean checkFormat (String filePath){  
 		String extension=null;
 		int position = filePath.lastIndexOf(".");
 		extension=filePath.substring(position+1);
-		System.out.println("format is "+extension);
-        if(extension.equals("txt")){
-        	isText=true;
-        	return true;
-        }
+	//	System.out.println("format is "+extension);
+		if(extension.equals("txt")){
+			isText=true;
+			return true;
+		}
 		for(int i=0;i<ListOfFormatSize;i++){
 			if(listOfFormat.get(i).equals(extension)){
 				isPhoto=true;
@@ -60,157 +54,86 @@ public class fileLoader
 		return false;
 	}
 
-	private void createAndDisplayGUI(String filePath)
+	void createAndDisplayGUI(String filePath) throws IOException
 	{
-		JFrame frame = new JFrame("file displayed");
+		String fileName="file displayed";
+		fileName=filePath.substring(filePath.lastIndexOf("\\")+1);
+		JFrame frame = new JFrame(fileName);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 		paintingPanel = new CustomPanel();
-		if(isPhoto){
-		System.out.println("now displaying photo");
-		}
-		if(isText){
-			System.out.println("now displaying text");
-		}
+
 		paintingPanel.setShowPhoto(isPhoto);
 		paintingPanel.setShowText(isText);
 		paintingPanel.setPath(filePath);
 
-		/*      final JButton startStopButton = new JButton("STOP");
-        startStopButton.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent ae)
-            {
-                if (timer.isRunning())
-                {
-                    startStopButton.setText("START");
-                    timer.stop();
-                }
-                else if (!timer.isRunning())
-                {
-                    startStopButton.setText("STOP");
-                    timer.start();
-                }
-            }
-        }); */
+		if(isPhoto){
+	//		System.out.println("now displaying photo");
+			BufferedImage bimg = ImageIO.read(new File(filePath));
+			int width = bimg.getWidth();
+			int height = bimg.getHeight();
+			paintingPanel.setWidth(width);
+			paintingPanel.setHeight(height);
+	//		System.out.println("width is "+width);
+	//		System.out.println("height is "+height);
+			frame.add(paintingPanel, BorderLayout.CENTER);
+			frame.pack();
+			frame.setLocationByPlatform(true);
+			frame.setVisible(true);
 
-		frame.add(paintingPanel, BorderLayout.CENTER);
-		//        frame.add(startStopButton, BorderLayout.PAGE_END);
-		frame.pack();
-		frame.setLocationByPlatform(true);
-		frame.setVisible(true);
-		//      timer = new Timer(100, timerAction);
-		//      timer.start();
+		}
+		if(isText){
+	//		System.out.println("now displaying text");
+			try {
+				content=readFile(filePath);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			JTextArea textArea = new JTextArea(content, 30, 40);
+			textArea.setPreferredSize(new Dimension(800, 600));
+			JScrollPane scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+					JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+			textArea.setLineWrap(true);
+			textArea.setEditable(false);
+			frame.add(scrollPane);
+			frame.pack();
+			frame.setLocationByPlatform(true);
+			frame.setVisible(true);
+		}
+
 	}
 
 	public void start(String filePath)
-	//public static void main(String... args)
 	{
 		SwingUtilities.invokeLater(new Runnable()
 		{
 			public void run()
 			{
-		  //     String filePath=args[0];
-		  //	String filePath="c:\\sp.png";
-		        listOfFormat.add("jpg");
+
+				listOfFormat.add("jpg");
 				listOfFormat.add("jpeg");
 				listOfFormat.add("png");
-		       if(checkFormat(filePath)==false){
-		   		JOptionPane.showMessageDialog(null,"Current doc format is not supported","Error",JOptionPane.INFORMATION_MESSAGE);   
-		       }
-		       else{
-		    	new fileLoader().createAndDisplayGUI(filePath);
-		       }
+				if(checkFormat(filePath)==false){
+					JOptionPane.showMessageDialog(null,"Current doc format is not supported","Error",JOptionPane.INFORMATION_MESSAGE);   
+				}
+				else{
+					try {
+						new fileLoader().createAndDisplayGUI(filePath);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			}
 		});
 	}
-	
-/*	  public static void main(String... args)
-	    {
-	        SwingUtilities.invokeLater(new Runnable()
-	        {
-	            public void run()
-	            {
-	                new PaintingExample().createAndDisplayGUI();
-	            }
-	        });
-	    }
-	*/
+
 	public void printPath(){
 		System.out.println("path is "+path);
 	}
 
-	
-}
-
-class CustomPanel extends JPanel
-{
-	private int x = 0;
-	private int y = 0;
-	public boolean displayPhoto=false;
-	public boolean displayText=false;
-	private String displayedPath=null;
-	private boolean showPhoto=false;
-	private boolean showText=false;
-	private Image picture = null;
-	private String content=null;
-	
-	public void setShowPhoto(boolean x){
-		showPhoto=x;
-	}
-	
-	public void setShowText(boolean x){
-		showText=x;
-	}
-	public void setPath(String p){
-		displayedPath=p;
-	}
-	@Override
-	public Dimension getPreferredSize()
-	{		
-		return (new Dimension(800, 600));
-	}
-
-	public void setPosition(int a, int b)
-	{
-		x = a;
-		y = b;
-		if (x <(getWidth() - 10) && y < (getHeight() - 10))
-			repaint();
-		else
-			System.out.println("Nothing is happening...");
-	}
-
-	@Override
-	public void paintComponent(Graphics g)
-	{
-		System.out.println(" path is "+displayedPath);	
-		
-		if(showPhoto==true){
-		picture = getImage(displayedPath);
-		Graphics2D g2 = (Graphics2D)g;	    
-		g2.drawImage(picture, 0, 0, 800,600, this);
-		
-		}
-		if(showText==true){
-		/*	super.paintComponent(g);
-			g.clearRect(0, 0, getWidth(), getHeight());
-			g.setColor(Color.MAGENTA);
-			g.fillOval(x, y, 10, 10);
-		*/	
-			Graphics2D g2 = (Graphics2D)g;	
-	 		try {
-	 			content=readFile(displayedPath);
-	 		} catch (IOException e) {
-	 			// TODO Auto-generated catch block
-	 			e.printStackTrace();
-	 		}
-	 		LinkedList<String> list=convertStringToLineString(content);
-	 		for(int i=0;i<list.size();i++){
-	 			g2.drawString(list.get(i),10,10*(i+1));
-	 		}
-		}
-	}
 	public String readFile(String filePath) throws IOException{
 		BufferedReader br = new BufferedReader(new FileReader(filePath));
 		try {
@@ -222,12 +145,76 @@ class CustomPanel extends JPanel
 				sb.append("\n");
 				line = br.readLine();
 			}
-
 			return sb.toString();
 		} finally {
 			br.close();
 		}
 	}
+}
+
+class CustomPanel extends JPanel
+{
+	private int x = 0;
+	private int y = 0;
+	private String displayedPath=null;
+	private boolean showPhoto=false;
+	private boolean showText=false;
+	private Image picture = null;
+	private String content=null;
+	private int width=0;
+	private int height=0;
+
+	public void setWidth(int x){
+		width=x;
+	}
+	public void setHeight(int x){
+		height=x;
+	}
+	public void setShowPhoto(boolean x){
+		showPhoto=x;
+	}
+
+	public void setShowText(boolean x){
+		showText=x;
+	}
+	public void setPath(String p){
+		displayedPath=p;
+	}
+	@Override
+	public Dimension getPreferredSize()
+	{		
+		if(showPhoto){
+			return (new Dimension(width, height));
+		}
+		if(showText){
+			return (new Dimension(800, 600));
+		}
+		return (new Dimension(800, 600));
+	}
+
+	public void setPosition(int a, int b)
+	{
+		x = a;
+		y = b;
+		if (x <(getWidth() - 10) && y < (getHeight() - 10))
+			repaint();
+		else
+	       System.out.println("Nothing is happening...");
+	}
+
+	@Override
+	public void paintComponent(Graphics g)
+	{
+	//	System.out.println(" path is "+displayedPath);	
+
+		//	if(showPhoto==true){
+		picture = getImage(displayedPath);
+		Graphics2D g2 = (Graphics2D)g;	    
+		g2.drawImage(picture, 0,0,getWidth(),getHeight(),this);
+
+	}
+
+
 	public Image getImage(String path){
 		Image tempImage=null;
 		try{
@@ -246,7 +233,7 @@ class CustomPanel extends JPanel
 		String[] parts = str.split("\n");
 		LinkedList<String> list=new LinkedList();
 
-		System.out.println("num of rows "+parts.length);
+	//	System.out.println("num of rows "+parts.length);
 		for(int i=0;i<parts.length;i++){
 			list.add(parts[i]);
 		}
