7c0c5b767b70e9092dfc8e523eadc0c0709229db
projectcs2103t <projectcs2103t@gmail.com>
2015-01-15 10:19:03 +0800

    Deleted useless api & analyzers

--- EagleEye/src/eagleeye/api/analyzer/EagleLocationAnalyzer.java
+++
@@ -1,8 +0,0 @@
-package eagleeye.api.analyzer;
-
-import java.util.List;
-import eagleeye.api.entities.EagleLocation;
-
-public interface EagleLocationAnalyzer {
-	public List<EagleLocation> getLocations();
-}
