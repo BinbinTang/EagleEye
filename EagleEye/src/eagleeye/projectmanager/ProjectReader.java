dd6099d6b8c4f0d7fdae446b92d8dca5775e04c3
BinbinTang <tangbbnus@gmail.com>
2015-02-07 12:07:45 +0800

    Create ProjectReader

---
+++ EagleEye/src/eagleeye/projectmanager/ProjectReader.java
@@ -0,0 +1,17 @@
+package eagleeye.projectmanager;
+
+import java.util.List;
+import java.util.Map;
+
+public class ProjectReader {
+	private String filePath;
+	public ProjectReader(String path)
+	{
+		filePath = path;
+	}
+	
+	public Map<String, List<List<String>>> readFile(){
+		Map<String, List<List<String>>> markedItem = new Map<String, List<List<String>>>();
+		return 
+	}
+}
