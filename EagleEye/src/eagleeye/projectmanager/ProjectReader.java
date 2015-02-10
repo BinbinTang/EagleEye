a72ad37dbb9e812f57f045d6047f69849be2bad7
BinbinTang <tangbbnus@gmail.com>
2015-02-11 01:43:54 +0800

    Finish Project Reader

--- EagleEye/src/eagleeye/projectmanager/ProjectReader.java
+++ EagleEye/src/eagleeye/projectmanager/ProjectReader.java
@@ -6,6 +6,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.HashMap;
 
@@ -14,20 +15,25 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 public class ProjectReader {
 	
 	private String filePath;
+	private HashMap<String, ArrayList<ArrayList<String>>> markedItem;
+	
 	public ProjectReader(String path)
 	{
 		filePath = path;
+		markedItem = new HashMap<String, ArrayList<ArrayList<String>>>();
 	}
 	
 	public HashMap<String, ArrayList<ArrayList<String>>> readFile() {
-		HashMap<String, ArrayList<ArrayList<String>>> markedItem = new HashMap<String, ArrayList<ArrayList<String>>>();
+		
 		
 		File fXmlFile = new File(filePath+File.separator+"markedFile.xml");
 		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -37,8 +43,9 @@ public class ProjectReader {
 			Document doc = dBuilder.parse(fXmlFile);
 			doc.getDocumentElement().normalize();
 			
+			NodeList nl = doc.getElementsByTagName("Plugin-Name");
 			if (doc.hasChildNodes())
-				executeChildNodes(doc.getChildNodes(), markedItem);
+				executeRootNodes(doc.getChildNodes().item(0));
 			
 		} catch (ParserConfigurationException e1) {
 			// TODO Auto-generated catch block
@@ -55,17 +62,72 @@ public class ProjectReader {
 		return markedItem;
 	}
 	
-	public void executeChildNodes(NodeList nodeList, HashMap<String, ArrayList<ArrayList<String>>> markedItem)
+	public void executeRootNodes(Node root)
 	{
-		for (int i=0; i<nodeList.getLength(); i++)
+		NodeList nl = root.getChildNodes();
+		Element plugin;
+		for (int i=0; i<nl.getLength(); i++)
 		{
-			Node plugin = nodeList.item(i);
+			plugin = (Element) nl.item(i);
+			String name = plugin.getAttribute("Plugin-Name");
+			ArrayList<ArrayList<String>> var = executePlugin(plugin);
+			markedItem.put(name, var);
 		}
+	}
+	
+	public ArrayList<ArrayList<String>> executePlugin(Element plugin)
+	{
+		NodeList nl = plugin.getChildNodes();
+		Element file;
+		if (nl.getLength() == 0)
+			return null;
+		ArrayList<ArrayList<String>> markedFile = new ArrayList<ArrayList<String>>();
+		markedFile.add(retrieveAttributesName((Element) nl.item(0)));
+		for (int i=0; i<nl.getLength();i++)
+		{
+			file = (Element) nl.item(i);
+			markedFile.add(retrieveFileContent(file));
+		}
+		return markedFile;
+	}
+	
+	public ArrayList<String> retrieveAttributesName(Element file)
+	{
+		ArrayList<String> attributesName = new ArrayList<String>();
+		NodeList nl = file.getChildNodes();
+		
+		for (int i=0; i<nl.getLength(); i++)
+		{
+			Node attributes = nl.item(i);
+			attributesName.add(attributes.getNodeName());
+		}
+		return attributesName;
+	}
+	
+	public ArrayList<String> retrieveFileContent(Element file)
+	{
+		ArrayList<String> fileContent = new ArrayList<String>();
+		NodeList nl = file.getChildNodes();
+		
+		for (int i=0; i<nl.getLength(); i++)
+		{
+			Node attribute = nl.item(i);
+			fileContent.add(attribute.getTextContent());
+		}
+		return fileContent;
 	}
 	
 	public static void main(String[] args)
 	{
 		ProjectReader reader = new ProjectReader("/Users/BinbinTang/Desktop");
-		reader.readFile(filePath);
-		}
+		HashMap<String, ArrayList<ArrayList<String>>> markedItem = reader.readFile();
+		
+		Iterator<String> iter = markedItem.keySet().iterator(); 
+        while (iter.hasNext()) {  
+            String key = iter.next(); 
+            System.out.println("Key1 = "+key);
+            ArrayList<ArrayList<String>> val = markedItem.get(key);
+            System.out.println(val.toString());
+        }
+	}
 }
