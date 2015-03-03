0d9f9bd167ffa1bcd7bbb2a55e8e16e423b5f859
zimeng <MACHERIE@192.168.1.101>
2015-03-03 16:26:24 +0800

    test

---
+++ EagleEye/src/eagleeye/test/testController.java
@@ -0,0 +1,48 @@
+
+package eagleeye.test;
+
+
+public class testController {
+
+
+
+
+
+public static void main(String[] args){
+
+test my=new test();
+
+ //   my.testFileFormatIdentifier();
+
+//my.testAudio();
+
+my.testDevice();
+
+my.testDirectory();
+
+        my.testFileEntity();
+
+   //     my.testPhoto();
+
+        my.testFilter();
+
+   //     my.testVideo();
+
+        my.testFormatDescription();
+
+     //   my.testFormatIdentifierManager();
+
+        my.testMTDPartitionDefinition();
+
+        my.testMTDDefinition();
+
+        my.testDBInsertTransaction();
+
+        my.testDBQueryController();
+
+}
+
+
+
+}
+
